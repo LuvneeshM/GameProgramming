53c19ab00c0d016a5b77dcc187866bfcc28c5ea6
Luvneesh Mugrai <lm3300@nyu.edu>
2017-02-18 03:54:08 -0500

    Has a reset, winner, a few instructions. Collision is still a bit wonky...

--- Homework_2/NYUCodebase/main.cpp
+++ Homework_2/NYUCodebase/main.cpp
@@ -25,6 +25,45 @@ using namespace std;
 
 SDL_Window* displayWindow;
 
+//gloabl stuff
+int winner = 0;
+bool gameOn = false;
+
+void DrawText(ShaderProgram *program, int fontTexture, std::string text, float size, float spacing) {
+	float texture_size = 1.0 / 16.0f;
+	std::vector<float> vertexData;
+	std::vector<float> texCoordData;
+	for (int i = 0; i < text.size(); i++) {
+		float texture_x = (float)(((int)text[i]) % 16) / 16.0f;
+		float texture_y = (float)(((int)text[i]) / 16) / 16.0f;
+		vertexData.insert(vertexData.end(), {
+			((size + spacing) * i) + (-0.5f * size), 0.5f * size,
+			((size + spacing) * i) + (-0.5f * size), -0.5f * size,
+			((size + spacing) * i) + (0.5f * size), 0.5f * size,
+			((size + spacing) * i) + (0.5f * size), -0.5f * size,
+			((size + spacing) * i) + (0.5f * size), 0.5f * size,
+			((size + spacing) * i) + (-0.5f * size), -0.5f * size,
+		});
+		texCoordData.insert(texCoordData.end(), {
+			texture_x, texture_y,
+			texture_x, texture_y + texture_size,
+			texture_x + texture_size, texture_y,
+			texture_x + texture_size, texture_y + texture_size,
+			texture_x + texture_size, texture_y,
+			texture_x, texture_y + texture_size,
+		});
+	}
+	glUseProgram(program->programID);
+	glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertexData.data());
+	glEnableVertexAttribArray(program->positionAttribute);
+	glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoordData.data());
+	glEnableVertexAttribArray(program->texCoordAttribute);
+	glBindTexture(GL_TEXTURE_2D, fontTexture);
+	glDrawArrays(GL_TRIANGLES, 0, text.size() * 6);
+	glDisableVertexAttribArray(program->positionAttribute);
+	glDisableVertexAttribArray(program->texCoordAttribute);
+}
+
 GLuint LoadTexture(const char *filePath) {
 	int w, h, comp;
 	unsigned char* image = stbi_load(filePath, &w, &h, &comp, STBI_rgb_alpha);
@@ -52,6 +91,8 @@ public:
 	bool movingX;
 	std::string type;
 	//float vertices[] = { -0.125, -0.5, 0.125, -0.5, 0.125, 0.5, -0.125, -0.5, 0.125, 0.5, -0.125, 0.5 };
+
+	Entity(){}
 
 	Entity(float cX, float cY, float w, float h, bool mY, bool mX, std::string t) : 
 		centerX(cX), centerY(cY), width(w), height(h), movingY(mY), movingX(mX), type(t){
@@ -103,15 +144,13 @@ public:
 	void collisionCheck(Entity r1){ //current is R2
 
 		//collide
-		if (!((r1.centerY - r1.height / 2 >= this->centerY + this->height / 2) || //r1 bot > r2 top
-			(r1.centerY + r1.height / 2 <= this->centerY - this->height / 2) || //r1 top < r2 bot
-			(r1.centerX - r1.width / 2 >= this->centerX + this->width / 2) || //r1 left > r2 right
-			(r1.centerX + r1.width / 2 <= this->centerX - this->width / 2) //r1 right < r2 left
-			)){
+		if (!(r1.centerY - r1.height / 2 >= this->centerY + this->height / 2) && //r1 bot > r2 top
+			!(r1.centerY + r1.height / 2 <= this->centerY - this->height / 2) && //r1 top < r2 bot
+			!(r1.centerX - r1.width / 2 >= this->centerX + this->width / 2) && //r1 left > r2 right
+			!(r1.centerX + r1.width / 2 <= this->centerX - this->width / 2) //r1 right < r2 left
+			){
 
 			//DELETE THIS
-
-
 			if (r1.type == "paddle"){
 				ofstream log;
 				log.open("log.txt");
@@ -144,8 +183,8 @@ public:
 			else if (r1.type == "paddle"){
 				//be specific please
 				//left/right
-				if ( ((r1.centerX + r1.width / 2 >= this->centerX - this->width / 2) || (r1.centerX - r1.width / 2 <= this->centerX + this->width / 2)) &&
-					(r1.centerY + r1.height/2 >= this->centerY + this->height/2) && (r1.centerY - r1.height/2 <= this->centerY - this->height/2) ){
+				if (((r1.centerX + r1.width / 2 >= this->centerX - this->width / 2) || (r1.centerX - r1.width / 2 <= this->centerX + this->width / 2)) &&
+					(r1.centerY + r1.height / 2 >= this->centerY + this->height / 2) && (r1.centerY - r1.height / 2 <= this->centerY - this->height / 2)){
 					this->speedX *= -1;
 				}
 				//hit bot/top
@@ -154,14 +193,20 @@ public:
 				}
 			}
 		}
+		//did I win
+		else if (this->type == "ball"){
+			//right wins 
+			if (this->centerX - this->width/2 <= -3.55f) {
+				winner = 1;
+				gameOn = false;
+			}
+			//left wins
+			else if (this->centerX + this->width/2 >= 3.55f){
+				winner = 2;
+				gameOn = false;
+			}
+		}
 	}
-	//if (!(leftPad.centerY - leftPad.height / 2 >= ball.yCoord + ball.height / 2) && //r1 bot > r2 top
-	//	!(leftPad.centerY + leftPad.height / 2 <= ball.yCoord - ball.height / 2) && //r1 top < r2 bot
-	//	!(leftPad.centerX - leftPad.width / 2 >= ball.xCoord + ball.width / 2) && //r1 left > r2 right
-	//	!(leftPad.centerX + leftPad.width / 2 <= ball.xCoord - ball.width / 2) //r1 right < r2 left
-	//	){
-	//	ball.xDir *= -1;
-	//}
 
 };
 /*
@@ -195,6 +240,13 @@ public:
 	}
 };
 */
+
+void setUp(Entity& leftPaddleE, Entity& rightPaddleE, Entity& ballE){
+	leftPaddleE = Entity(-3.125f, 0.0f, 0.5f, 1.0f, true, false, "paddle");
+	rightPaddleE = Entity(3.125f, 0.0f, 0.5f, 1.0f, true, false, "paddle");
+	ballE = Entity(0.0f, 0.0f, 0.2f, 0.2f, true, true, "ball");
+}
+
 int main(int argc, char *argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
@@ -206,16 +258,15 @@ int main(int argc, char *argv[])
 	#endif
 
 		//left Paddle
-		Entity leftPaddleE(-3.125f, 0.0f, 0.5f, 1.0f, true, false, "paddle");
+		Entity leftPaddleE;
 		GLuint leftPaddle = LoadTexture(RESOURCE_FOLDER"PaddleSprite.png");
 		//right Paddl
-		Entity rightPaddleE(3.125f, 0.0f, 0.5f, 1.0f, true, false, "paddle");
+		Entity rightPaddleE;
 		GLuint rightPaddle = LoadTexture(RESOURCE_FOLDER"PaddleSprite.png");
 		//ball
-		Entity ballE(0.0f, 0.0f, 0.2f, 0.2f, true, true, "ball");
-		ballE.speedX = -0.50f;//rand() % (3 - 1 + 1) + 1;
-		ballE.speedY = -0.50f;//rand() % (3 - 1 + 1) + 1;
-		GLuint ballSprite = LoadTexture(RESOURCE_FOLDER"BallSprite.png");
+		Entity ballE;
+		GLuint ballSprite = LoadTexture(RESOURCE_FOLDER"BallSprite.png");	
+		
 		//walls
 		GLuint wallSprite = LoadTexture(RESOURCE_FOLDER"WallSprite.png");
 		Entity topWallE(0.0f, 1.95f, 3.55 * 2, 0.1, false, false, "wall");
@@ -223,7 +274,9 @@ int main(int argc, char *argv[])
 		
 		//std::vector<Entity> allEntity = { leftPaddleE, rightPaddleE, ballE, topWallE, botWallE };
 		
-		bool gameOn = false;
+		//font
+		GLuint font = LoadTexture(RESOURCE_FOLDER"font1.png");
+		string whoWon = "";
 
 		float lastTick = 0;
 
@@ -263,10 +316,13 @@ int main(int argc, char *argv[])
 					rightPaddleE.speedY = -1.0f;
 					//rightPad.paddleSpeed = -1.0f;
 				}
-				//start game
+				//start and reset game game
 				if (event.key.keysym.scancode == SDL_SCANCODE_SPACE){
 					if (!gameOn){
 						gameOn = true;
+						setUp(leftPaddleE, rightPaddleE, ballE);
+						ballE.speedX = -1.50f;//rand() % (3 - 1 + 1) + 1;
+						ballE.speedY = -0.50f;//rand() % (3 - 1 + 1) + 1;
 					}
 				}
 			}
@@ -288,6 +344,7 @@ int main(int argc, char *argv[])
 
 		glClear(GL_COLOR_BUFFER_BIT);
 
+
 		//walls
 		//top
 		topWallE.update(elapsed/*, allEntity*/);
@@ -296,129 +353,160 @@ int main(int argc, char *argv[])
 		botWallE.update(elapsed/*, allEntity*/);
 		botWallE.draw(program, wallSprite);
 
-		//left paddle entity
-		leftPaddleE.update(elapsed/*, allEntity*/);
+		if (gameOn){
+			//updates
+			//paddles
+			leftPaddleE.update(elapsed/*, allEntity*/);
+			rightPaddleE.update(elapsed/*, allEntity*/);
+			//ball
+			ballE.update(elapsed/*, allEntity*/);
+
+			//udpate p1
+			//ball collision
+			ballE.collisionCheck(topWallE);
+			ballE.collisionCheck(botWallE);
+			ballE.collisionCheck(leftPaddleE);
+			ballE.collisionCheck(rightPaddleE);
+			//update p2
+			//player collision
+			leftPaddleE.collisionCheck(topWallE);
+			leftPaddleE.collisionCheck(botWallE);
+			rightPaddleE.collisionCheck(topWallE);
+			rightPaddleE.collisionCheck(botWallE);
+		}
 		//draw
 		leftPaddleE.draw(program, leftPaddle);
-		//glBindTexture(GL_TEXTURE_2D, leftPaddle);
-		//float leftPaddleVert[] = { -0.125, -0.5, 0.125, -0.5, 0.125, 0.5, -0.125, -0.5, 0.125, 0.5, -0.125, 0.5 };
-		//glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, leftPaddleVert);
-		//glEnableVertexAttribArray(program.positionAttribute);
-		////show all of image
-		//float leftPaddleTextCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
-		//glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, leftPaddleTextCoords);
-		//glEnableVertexAttribArray(program.texCoordAttribute);
-		//program.setModelMatrix(leftPaddleE.modelMatrix);
-		//glDrawArrays(GL_TRIANGLES, 0, 6);
-		//glDisableVertexAttribArray(program.positionAttribute);
-		//glDisableVertexAttribArray(program.texCoordAttribute);
-
-
-		/*
-		//left paddle
-		glBindTexture(GL_TEXTURE_2D, leftPaddle);
-		//set vertices
-		float leftPaddleVert[] = { -0.125, -0.5, 0.125, -0.5, 0.125, 0.5, -0.125, -0.5, 0.125, 0.5, -0.125, 0.5 };
-		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, leftPaddleVert);
-		glEnableVertexAttribArray(program.positionAttribute);
-		//show all of image
-		float leftPaddleTextCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
-		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, leftPaddleTextCoords);
-		glEnableVertexAttribArray(program.texCoordAttribute);
-
-		modelLeftPadd.identity();
-		//keep track of center
-		leftPad.centerY += leftPad.paddleSpeed * elapsed;
-		modelLeftPadd.Translate(leftPad.centerX, leftPad.centerY, 0.0f);
-
-		program.setModelMatrix(modelLeftPadd);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		
-		glDisableVertexAttribArray(program.positionAttribute);
-		glDisableVertexAttribArray(program.texCoordAttribute);
-		*/
-		//right paddle
-		rightPaddleE.update(elapsed/*, allEntity*/);
 		rightPaddleE.draw(program, rightPaddle);
-		/*glBindTexture(GL_TEXTURE_2D, rightPaddle);
-		//vertices
-		float rightPaddleVert[] = { -0.125, -0.5, 0.125, -0.5, 0.125, 0.5, -0.125, -0.5, 0.125, 0.5, -0.125, 0.5 };
-		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, rightPaddleVert);
-		glEnableVertexAttribArray(program.positionAttribute);
-		//image
-		float rightPaddleTextCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
-		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, rightPaddleTextCoords);
-		glEnableVertexAttribArray(program.texCoordAttribute);
+		ballE.draw(program, ballSprite);
 
-		modelRightPadd.identity();
-		//track center
-		rightPad.centerY += rightPad.paddleSpeed * elapsed;
-		modelRightPadd.Translate(rightPad.centerX, rightPad.centerY, 0.0f);
+		//winner?
+		if (!gameOn){
+			modelMatrix.identity();
+			modelMatrix.Translate(-2.5f, 1.5f, 0.0f);
+			program.setModelMatrix(modelMatrix);
+			string help = "Press Space to Start";
+			DrawText(&program, font, help, 0.25f, 0);
+			modelMatrix.identity();
+			modelMatrix.Translate(-2.5f, -1.0f, 0.0f);
+			program.setModelMatrix(modelMatrix);
+			if (winner == 1){
+				whoWon = "Left Player Won";
+				DrawText(&program, font, whoWon, 0.25f, 0);
+			}
+			else if (winner == 2){
+				whoWon = "Right Player Won";
+				DrawText(&program, font, whoWon, 0.25f, 0);
+			}
 
-		program.setModelMatrix(modelRightPadd);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		}
 
-		glDisableVertexAttribArray(program.positionAttribute);
-		glDisableVertexAttribArray(program.texCoordAttribute);
-		*/
-		//udpate p1
-		//ball collision
-		ballE.collisionCheck(topWallE);
-		ballE.collisionCheck(botWallE);
-		ballE.collisionCheck(leftPaddleE);
-		ballE.collisionCheck(rightPaddleE);
-		//update p2
-		ballE.update(elapsed/*, allEntity*/);
-		ballE.draw(program, ballSprite); 
-		//player collision
-		leftPaddleE.collisionCheck(topWallE);
-		leftPaddleE.collisionCheck(botWallE);
-		rightPaddleE.collisionCheck(topWallE);
-		rightPaddleE.collisionCheck(botWallE);
+			//left paddle entity
+			//draw
+			
+			//glBindTexture(GL_TEXTURE_2D, leftPaddle);
+			//float leftPaddleVert[] = { -0.125, -0.5, 0.125, -0.5, 0.125, 0.5, -0.125, -0.5, 0.125, 0.5, -0.125, 0.5 };
+			//glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, leftPaddleVert);
+			//glEnableVertexAttribArray(program.positionAttribute);
+			////show all of image
+			//float leftPaddleTextCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+			//glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, leftPaddleTextCoords);
+			//glEnableVertexAttribArray(program.texCoordAttribute);
+			//program.setModelMatrix(leftPaddleE.modelMatrix);
+			//glDrawArrays(GL_TRIANGLES, 0, 6);
+			//glDisableVertexAttribArray(program.positionAttribute);
+			//glDisableVertexAttribArray(program.texCoordAttribute);
+
+
+			/*
+			//left paddle
+			glBindTexture(GL_TEXTURE_2D, leftPaddle);
+			//set vertices
+			float leftPaddleVert[] = { -0.125, -0.5, 0.125, -0.5, 0.125, 0.5, -0.125, -0.5, 0.125, 0.5, -0.125, 0.5 };
+			glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, leftPaddleVert);
+			glEnableVertexAttribArray(program.positionAttribute);
+			//show all of image
+			float leftPaddleTextCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+			glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, leftPaddleTextCoords);
+			glEnableVertexAttribArray(program.texCoordAttribute);
+
+			modelLeftPadd.identity();
+			//keep track of center
+			leftPad.centerY += leftPad.paddleSpeed * elapsed;
+			modelLeftPadd.Translate(leftPad.centerX, leftPad.centerY, 0.0f);
+
+			program.setModelMatrix(modelLeftPadd);
+			glDrawArrays(GL_TRIANGLES, 0, 6);
+
+			glDisableVertexAttribArray(program.positionAttribute);
+			glDisableVertexAttribArray(program.texCoordAttribute);
+			*/
+			//right paddle
+			/*glBindTexture(GL_TEXTURE_2D, rightPaddle);
+			//vertices
+			float rightPaddleVert[] = { -0.125, -0.5, 0.125, -0.5, 0.125, 0.5, -0.125, -0.5, 0.125, 0.5, -0.125, 0.5 };
+			glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, rightPaddleVert);
+			glEnableVertexAttribArray(program.positionAttribute);
+			//image
+			float rightPaddleTextCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+			glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, rightPaddleTextCoords);
+			glEnableVertexAttribArray(program.texCoordAttribute);
+
+			modelRightPadd.identity();
+			//track center
+			rightPad.centerY += rightPad.paddleSpeed * elapsed;
+			modelRightPadd.Translate(rightPad.centerX, rightPad.centerY, 0.0f);
+
+			program.setModelMatrix(modelRightPadd);
+			glDrawArrays(GL_TRIANGLES, 0, 6);
+
+			glDisableVertexAttribArray(program.positionAttribute);
+			glDisableVertexAttribArray(program.texCoordAttribute);
+			*/
+	
+			//ball
+			//glBindTexture(GL_TEXTURE_2D, ballSprite);
+			////vertices
+			//float ballV[] = { -0.1, -0.1, 0.1, -0.1, 0.1, 0.1, -0.1, -0.1, 0.1, 0.1, -0.1, 0.1 };
+			//glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, ballV);
+			//glEnableVertexAttribArray(program.positionAttribute);
+			////uv
+			//float ballTextCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+			//glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, ballTextCoords);
+			//glEnableVertexAttribArray(program.texCoordAttribute);
+			//
+			//modelBall.identity();
+			//
+			////collision checks
+			////left paddle
+			///*if (!(leftPad.centerY - leftPad.height / 2 >= ball.yCoord + ball.height / 2) && //r1 bot > r2 top
+			//	!(leftPad.centerY + leftPad.height / 2 <= ball.yCoord - ball.height / 2) && //r1 top < r2 bot
+			//	!(leftPad.centerX - leftPad.width / 2 >= ball.xCoord + ball.width / 2) && //r1 left > r2 right
+			//	!(leftPad.centerX + leftPad.width / 2 <= ball.xCoord - ball.width / 2) //r1 right < r2 left
+			//	){
+			//	ball.xDir *= -1;
+			//}*/
+
+			////collision with screen boundaries
+			////lower wall || upper wall
+			//if ((ball.yCoord + ball.height / 2) >= 2.0 || (ball.yCoord - ball.height / 2) <= -2.0f){
+			//	ball.yDir *= -1;
+			//	//ball.vel += ball.accel; //make it a bit faster
+			//} 
+
+			//if (ball.xCoord <= -3.55f || ball.xCoord >= 3.55f){
+			//	ball.xDir *= -1;
+			//	ball.yCoord = 0;
+			//	ball.xCoord = 0;
+			//}
+
+			////no collision
+			////just keep moving
+			//ball.movement(elapsed);
+			//modelBall.Translate(ball.xCoord, ball.yCoord, 0.0f);
+			//program.setModelMatrix(modelBall);
+			//glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		
-
-		//glBindTexture(GL_TEXTURE_2D, ballSprite);
-		////vertices
-		//float ballV[] = { -0.1, -0.1, 0.1, -0.1, 0.1, 0.1, -0.1, -0.1, 0.1, 0.1, -0.1, 0.1 };
-		//glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, ballV);
-		//glEnableVertexAttribArray(program.positionAttribute);
-		////uv
-		//float ballTextCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
-		//glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, ballTextCoords);
-		//glEnableVertexAttribArray(program.texCoordAttribute);
-		//
-		//modelBall.identity();
-		//
-		////collision checks
-		////left paddle
-		///*if (!(leftPad.centerY - leftPad.height / 2 >= ball.yCoord + ball.height / 2) && //r1 bot > r2 top
-		//	!(leftPad.centerY + leftPad.height / 2 <= ball.yCoord - ball.height / 2) && //r1 top < r2 bot
-		//	!(leftPad.centerX - leftPad.width / 2 >= ball.xCoord + ball.width / 2) && //r1 left > r2 right
-		//	!(leftPad.centerX + leftPad.width / 2 <= ball.xCoord - ball.width / 2) //r1 right < r2 left
-		//	){
-		//	ball.xDir *= -1;
-		//}*/
-
-		////collision with screen boundaries
-		////lower wall || upper wall
-		//if ((ball.yCoord + ball.height / 2) >= 2.0 || (ball.yCoord - ball.height / 2) <= -2.0f){
-		//	ball.yDir *= -1;
-		//	//ball.vel += ball.accel; //make it a bit faster
-		//} 
-
-		//if (ball.xCoord <= -3.55f || ball.xCoord >= 3.55f){
-		//	ball.xDir *= -1;
-		//	ball.yCoord = 0;
-		//	ball.xCoord = 0;
-		//}
-
-		////no collision
-		////just keep moving
-		//ball.movement(elapsed);
-		//modelBall.Translate(ball.xCoord, ball.yCoord, 0.0f);
-		//program.setModelMatrix(modelBall);
-		//glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		program.setProjectionMatrix(projectionMatrix);
 		program.setViewMatrix(viewMatrix);
